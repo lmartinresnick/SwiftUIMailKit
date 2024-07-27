de7b7470204c7b10c225ba238d2af52256b0ccac
Luke Martin-Resnick <63889477+lmartinresnick@users.noreply.github.com>
2024-07-27 13:01:25 -0700

    refactor: update public inits

--- Sources/SwiftUIMailKit/Dependencies/MailViewConfig.swift
+++ Sources/SwiftUIMailKit/Dependencies/MailViewConfig.swift
@@ -11,13 +11,26 @@ public struct MailDetails {
     public var recipient: String? // setting this will override singleton recipient variable in EmailComposer
     public var subject: String
     public var body: String?
+
+    public init(recipient: String? = nil, subject: String, body: String? = nil) {
+        self.recipient = recipient
+        self.subject = subject
+        self.body = body
+    }
 }
 
 public struct MailViewConfig {
-    public var details: MailDetails = MailDetails(subject: "")
-    public var open: Bool = false
-
+    public var details: MailDetails
+    public var open: Bool
     private let composer = EmailComposer()
+
+    public init(
+        details: MailDetails = MailDetails(subject: ""),
+        open: Bool = false
+    ) {
+        self.details = details
+        self.open = open
+    }
 
     public func handleRequest() -> EmailType {
         composer.handleEmailRequest(details: details)
